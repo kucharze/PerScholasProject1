7ab109c596c931f84f35ac541ca440b6e695b196
Zachary Kuchar <zacharyk323@live.com>
2023-06-26 16:14:29 -0400

    initial functions for display hand

--- app.js
+++ app.js
@@ -12,13 +12,21 @@ class Player {
 
   //Functions
   //Draw a card from the deck
+  drawCard() {
+    this.hand.push(this.deck.dealACard());
+  }
   //Play a card from hand
   // - If legal to play play it
   // - if not get an alert that you can't play it
   // - If 8 show a screen to change the suite settings
+  playCard() {}
+
   //Display cards to screen
   // - Show the picture for each card
   // - If time allows, add animations
+  displayHand() {
+    let hand = document.querySelector(".player");
+  }
 }
 
 //Class for computer
@@ -35,14 +43,26 @@ class Computer {
 
   //Functions
   //Draw a card from the deck
+  drawCard() {
+    this.hand.push(this.deck.dealACard());
+  }
+
   //Play a card from hand
   //--Play the first card available
   // ----- For simplicty if 8 is played keep suite as the suite of the 8
   // --------- If time allows create logic to change suite for computer accodingly
   //--Draw a card if it can't find a card to play
+  playCard() {}
+
   //Display cards to screen
   // --Simply show card backs
   // -- If time allows show animations
+  displayHand() {
+    let hand = document.querySelector(".player");
+    for (let i = 0; i < this.hand.length; i++) {
+      console.log(this.hand);
+    }
+  }
 }
 
 class Card {
@@ -51,9 +71,12 @@ class Card {
   // - Value: number of the card or jack, queen, king, ace
   // Suite: heart, diamond, club, spade
   // Image: the link for what image to display for the card
+  // Click
   constructor(value, suite) {
     this.suite = suite;
     this.value = value;
+    //Will pick out image based on suite and value
+    this.image = "";
   }
 }
 
@@ -75,6 +98,7 @@ class Deck {
   shuffle() {
     //randomly swap cards araound in deck
   }
+
   // - Reset the deck if we run low on cards
   remake() {
     let suites = ["S", "J", "D", "H"];
@@ -125,8 +149,9 @@ class CardPile {
 
 //Game loop functions and parameters
 let deck = new Deck();
+let pile = new CardPile();
 
-console.log(deck.deckList);
+// console.log(deck.deckList);
 
 //Take turns
 const processTurns = () => {
